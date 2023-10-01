156c2291b4471c592e3febb2202d521c4bfb04f5
dhiazjr123 <118143683+dhiazjr123@users.noreply.github.com>
2023-10-01 14:57:44 +0700

    Add files via upload

---
+++ MallardDuck.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice.oop;
+
+/**
+ *
+ * @author dhiaz
+ */
+public class MallardDuck extends Duck {
+    
+    public MallardDuck() {
+        
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+          
+    }
+    
+    @Override
+    public void display() {
+        System.out.println("I'am a real Mallard Duck"); 
+    }  
+}
